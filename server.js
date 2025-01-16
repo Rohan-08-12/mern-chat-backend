3e5b2eea3519fda0e0dc0313acadb828e521c561
Rohan-08-12 <rohannayyer12@gmail.com>
2025-01-16 14:25:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app=express();
 const server=http.createServer(app);
 const io=socketio(server,{
     cors:{
-        origin:['http://localhost:5173'],
+        origin:['https://merniochat.netlify.app'],
         methods:["GET","POST"],
         credentials:true,
     }
